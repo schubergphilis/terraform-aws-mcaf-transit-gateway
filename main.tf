46e4e41314ff7899b4818e430c95b4c340c4b3d6
Jorrit Elfferich <jelfferich@schubergphilis.com>
2024-08-22 11:34:33 +0200

    check on each.value.outside_ip_address_type

--- main.tf
+++ main.tf
@@ -262,7 +262,7 @@ resource "aws_vpn_connection" "default" {
   customer_gateway_id                     = aws_customer_gateway.default[each.key].id
   outside_ip_address_type                 = each.value.outside_ip_address_type
   transit_gateway_id                      = aws_ec2_transit_gateway.default.id
-  transport_transit_gateway_attachment_id = each.value.dx_gateway_id != null ? data.aws_ec2_transit_gateway_dx_gateway_attachment.default[each.key].id : null
+  transport_transit_gateway_attachment_id = each.value.outside_ip_address_type == "PrivateIpv4" ? data.aws_ec2_transit_gateway_dx_gateway_attachment.default[each.key].id : null
   type                                    = "ipsec.1"
 
   tunnel1_dpd_timeout_action           = each.value.tunnel1_options.dpd_timeout_action
