994266e8d3ce91e6b11066ce6f43d97ea9cbb31a
Jorrit Elfferich <jelfferich@schubergphilis.com>
2024-08-22 11:27:37 +0200

    data source when type is set

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_ec2_transit_gateway_dx_gateway_attachment" "default" {
-  for_each = { for key, connection in var.vpn_connection : key => connection if connection.dx_gateway_id != null }
+  for_each = { for key, connection in var.vpn_connection : key => connection if connection.outside_ip_address_type == "PrivateIpv4" }
 
   transit_gateway_id = aws_ec2_transit_gateway.default.id
   dx_gateway_id      = each.value.dx_gateway_id
